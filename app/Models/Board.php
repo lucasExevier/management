948db1323b7977be0aeca66140d9c78d4e983941
BAGOMBEKA-JOB-DEV <bagombekajob@gmail.com>
2025-11-15 13:08:29 +0300

    Create tasks, boards, projects, users models

---
+++ app/Models/Board.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Board extends Model
+{
+    protected $fillable = [
+        'name',
+        'description',
+        'project_id',
+        'position',
+    ];
+
+    public function project(): BelongsTo
+    {
+        return $this->belongsTo(Project::class);
+    }
+
+    public function tasks(): HasMany
+    {
+        return $this->hasMany(Task::class);
+    }
+}
